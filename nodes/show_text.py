d903a2854444883d1711ba0f9537761d63f1368c
GrailGreg <gregory@goatgames.com>
2025-05-13 14:28:45 +0800

    Added show_text node

---
+++ nodes/show_text.py
@@ -0,0 +1,17 @@
+from .nodes.node_64 import SaveImage64
+
+from .nodes.show_text import ShowText64
+
+NODE_CLASS_MAPPINGS = {
+    "SaveImage64": SaveImage64,
+    "ShowText64": ShowText64, 
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "SaveImage64": "Save Image with Base64 Output",
+    "ShowText64": "Base64 Show & Copy Text", 
+}
+
+WEB_DIRECTORY = "js"
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
